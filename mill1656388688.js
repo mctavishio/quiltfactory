302559a11937a80479f368be86981048c4961061
mctavish <kathy@mctavish.io>
2022-06-28 00:25:26 -0500

    add in block and frame files

--- mill1656388688.js
+++ mill1656388688.js
@@ -10,32 +10,33 @@ const datetimestr = datetime.toDateString();
 const datetimeISOstr = datetime.toISOString();
 const filmdir = prefix+timestampnow;
 const pigments= {
-//	black: "#191918",
+	black: "#191918",
 	white: "#fcfbe3",
 	blue: "#006699",
 	red: "#9a0000",
-//	yellow: "#ffcc00",
-	yellow: "#191918",
+	yellow: "#ffcc00",
 	gray: "#898988",
-//	darkgray: "#4b4b44"
-	black: "#4b4b44"
+	darkgray: "#4b4b44"
 };
+const pigmentset = {
+	color1: pigments.black,
+	color2: pigments.white,
+	color3: pigments.gray,
+	color4: pigments.blue,
+}
 const pigmentsets = {
-	bwxxxx_041200000000: tools.reifyWeightedArray([ [pigments.black,4, "black"], [pigments.white,12,"white"], [pigments.gray, 0,"gray"], [pigments.red, 0,"red"], [pigments.yellow, 0,"yellow"], [pigments.blue, 0,"blue"]]),
-	bwxxxx_120400000000: tools.reifyWeightedArray([ [pigments.black,12, "black"], [pigments.white,4,"white"], [pigments.gray, 0,"gray"], [pigments.red, 0,"red"], [pigments.yellow, 0,"yellow"], [pigments.blue, 0,"blue"]]),
-	bwxrxx_021200010000: tools.reifyWeightedArray([ [pigments.black,2, "black"], [pigments.white,12,"white"], [pigments.gray, 0,"gray"], [pigments.red, 1,"red"], [pigments.yellow, 1,"yellow"], [pigments.blue, 0,"blue"]]),
-	bwxrxx_120200010000: tools.reifyWeightedArray([ [pigments.black,12, "black"], [pigments.white,2,"white"], [pigments.gray, 0,"gray"], [pigments.red, 1,"red"], [pigments.yellow, 2,"yellow"], [pigments.blue, 0,"blue"]]),
-	bwxryx_120200010100: tools.reifyWeightedArray([ [pigments.black,12, "black"], [pigments.white,2,"white"], [pigments.gray, 0,"gray"], [pigments.red, 1,"red"], [pigments.yellow, 2,"yellow"], [pigments.blue, 0,"blue"]]),
-};
-console.log(`pigmentsets = ${JSON.stringify(pigmentsets)}`);
+	p1: tools.reifyWeightedArray([[pigmentset.color1,6],[pigmentset.color2,2],[pigmentset.color3,2],[pigmentset.color4,1]]),
+	p2: tools.reifyWeightedArray([[pigmentset.color1,0],[pigmentset.color2,12],[pigmentset.color3,1],[pigmentset.color4,1]]),
+	p3: tools.reifyWeightedArray([[pigmentset.color1,2],[pigmentset.color2,2],[pigmentset.color3,2],[pigmentset.color4,4]]),
+}
 const blockpigments = [0,1,2,3].map( r => {
 	let row = [0,1,2,3].map( c => {
-		let f = () => pigmentsets.bwxrxx_120200010000[tools.randominteger(0,pigmentsets.bwxrxx_120200010000.length)]; 
+		let f = () => pigmentsets.p1[tools.randominteger(0,pigmentsets.p1.length)]; 
 		if( (c===1 || c===2) && (r===1 || r===2) ) { 
-			f =  () => pigmentsets.bwxrxx_021200010000[tools.randominteger(0,pigmentsets.bwxrxx_021200010000.length)];  
+			f =  () => pigmentsets.p2[tools.randominteger(0,pigmentsets.p2.length)];  
 		}
 		else if((c===0 || c===3) && (r===0 || r===3)) {
-			f =  () => pigmentsets.bwxryx_120200010100[tools.randominteger(0,pigmentsets.bwxryx_120200010100.length)];  
+			f =  () => pigmentsets.p3[tools.randominteger(0,pigmentsets.p3.length)];  
 		}
 		return f;
 	});
@@ -58,6 +59,7 @@ let p = {
 	min: 1080,
 	max: 1080,
 	dx: 54, dy: 54,
+	blockdx: 1080/4, blockdy: 1080/4,
 	nblocks: 5,
 	nblockcols: 5,
 	nblockrows: 5,
@@ -70,14 +72,7 @@ const nrotations = 4;
 		file = count.toString().padStart(6, "0") + ".pdf";
 		let info = {id:millfile,timestamp: datetimestr,directory:filmdir,npages:nticks,Author:"mctavish",Subject:"generative grid series",Keywords: "net.art, webs, quilts, networks, generative, algorithmic" };
 		++count;
-		let filmfile = `${filmdir}/frame${file}`;
-		let doc = new PDFDocument(
-		{ 
-			size: [p.width,p.height],
-			info: info,
-		});
-		doc.pipe(fs.createWriteStream(filmfile));
-		doc.rect(0, 0, p.width, p.height).fillColor(pigments.white).fill();
+
 		let blocks = [0,1,2,3,4].map( b => {
 			return block = [0,1,2,3].map( r => [0,1,2,3].map( c => blockpigments[r][c]() ) );
 		});
@@ -99,8 +94,40 @@ const nrotations = 4;
 			});
 			return [b,rot1,rot2,rot3,b];
 		});
-		let dx = p.dx;
-		let dy = p.dy;
+
+		let dx = p.blockdx;
+		let dy = p.blockdy;
+		[...Array(p.nblockrows).keys()].forEach( nblockrow => {
+			let blockfile = `${filmdir}/block${file}_${nblockrow}`;
+			let doc = new PDFDocument(
+			{ 
+				size: [p.width,p.height],
+				info: info,
+			});
+			doc.pipe(fs.createWriteStream(blockfile));
+			doc.rect(0, 0, p.width, p.height).fillColor(pigments.white).fill();
+				[0,1,2,3].forEach( r => {
+				[0,1,2,3].forEach( c => {
+					let color = blocksrotations[nblockrow][0][r][c]; 
+					let x = r*dx + c*dx;
+					let y = c*dy + r*dy;  
+					doc.rect(x,y,dx,dy).strokeOpacity(0).fillColor(color).fill();
+				});
+			});
+			doc.end();
+		});
+
+		let filmfile = `${filmdir}/frame${file}`;
+		doc = new PDFDocument(
+		{ 
+			size: [p.width,p.height],
+			info: info,
+		});
+		doc.pipe(fs.createWriteStream(filmfile));
+		doc.rect(0, 0, p.width, p.height).fillColor(pigments.white).fill();
+
+		dx = p.dx;
+		dy = p.dy;
 		[...Array(p.nblockrows).keys()].forEach( nblockrow => {
 		[...Array(p.nblockcols).keys()].forEach( nblockcol => {
 			[0,1,2,3].forEach( r => {
